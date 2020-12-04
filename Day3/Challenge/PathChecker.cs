1c33b07dd0ed66ce8bd4721c598caf00d99f5282
Jerzy Kruszewski <jerzyj.kruszewski@gmail.com>
2020-12-04 16:08:23 +0100

    Added Day 3 challenges solver

---
+++ Day3/Challenge/PathChecker.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Day3.Challenge
+{
+    public class PathChecker
+    {
+        private static readonly IList<string> _map;
+
+        static PathChecker()
+        {
+            _map = new List<string>();
+
+            using (StreamReader reader = new StreamReader("./Challenge/input.txt", Encoding.UTF8))
+            {
+                while (reader.Peek() >= 0)
+                {
+                    _map.Add(reader.ReadLine());
+                }
+            }
+        }
+
+        public static int CountTreesOnPath(int right, int bottom)
+        {
+            int xCord = 0;
+            int yCord = 0;
+            int trees = 0;
+
+            while (yCord < _map.Count)
+            {
+                if (_map[yCord][xCord] == '#')
+                {
+                    trees++;
+                }
+
+                xCord = (xCord + right) % _map[yCord].Length;
+                yCord += bottom;
+            }
+
+            return trees;
+        }
+
+        public static long MultiplyTreesInPaths(params Tuple<int, int>[] rightBottomPairs)
+        {
+            long result = 1;
+
+            foreach (Tuple<int, int> item in rightBottomPairs)
+            {
+                result *= CountTreesOnPath(item.Item1, item.Item2);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AdventOfCode2020.Day3.Challenge;
+
+namespace AdventOfCode2020.Day3
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine($"First Challenge: {PathChecker.CountTreesOnPath(3, 1)}");
+            Console.WriteLine(@$"Second Challenge: {PathChecker.MultiplyTreesInPaths(
+                new Tuple<int, int>(1, 1), new Tuple<int, int>(3, 1), new Tuple<int, int>(5, 1),
+                new Tuple<int, int>(7, 1), new Tuple<int, int>(1, 2))}");
+        }
+    }
+}
