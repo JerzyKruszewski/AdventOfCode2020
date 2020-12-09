893c1ef4422bd9fd5937db80d505ea1b9345dc1e
Jerzy Kruszewski <jerzyj.kruszewski@gmail.com>
2020-12-09 12:58:36 +0100

    Added day 9 challenges solver

---
+++ Day9/Challenge/XMASEncription.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Day9.Challenge
+{
+    public class XMASEncription
+    {
+        private static readonly IList<long> _encriptionSequence;
+
+        static XMASEncription()
+        {
+            _encriptionSequence = new List<long>();
+
+            PopulateEncriptionSequence();
+        }
+
+        private static void PopulateEncriptionSequence()
+        {
+            using (StreamReader reader = new StreamReader("./Challenge/input.txt", Encoding.UTF8))
+            {
+                while (reader.Peek() >= 0)
+                {
+                    _encriptionSequence.Add(long.Parse(reader.ReadLine()));
+                }
+            }
+        }
+
+        public static long CheckEncription(int preambleLength)
+        {
+            List<long> preamble = _encriptionSequence.Take(preambleLength).ToList();
+
+            foreach (long encriptionValue in _encriptionSequence.Skip(preambleLength))
+            {
+                if (!FindSum(encriptionValue, preamble))
+                {
+                    return encriptionValue;
+                }
+
+                preamble.RemoveAt(0);
+                preamble.Add(encriptionValue);
+            }
+
+            return long.MinValue;
+        }
+
+        private static bool FindSum(long encriptionValue, List<long> preamble)
+        {
+            for (int i = 0; i < preamble.Count - 1; i++)
+            {
+                for (int j = 1; j < preamble.Count; j++)
+                {
+                    if (preamble[i] + preamble[j] == encriptionValue)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public static long FindSumOfSequenceGapWithSum(long expectedSum)
+        {
+            List<long> sequence = new List<long>();
+
+            for (int i = 0; i < _encriptionSequence.Count; i++)
+            {
+                foreach (long enctiptionValue in _encriptionSequence.Skip(i))
+                {
+                    if (enctiptionValue == expectedSum)
+                    {
+                        sequence.Clear();
+                        continue;
+                    }
+
+                    sequence.Add(enctiptionValue);
+                    long sumOfSequence = sequence.Sum();
+
+                    if (sumOfSequence == expectedSum)
+                    {
+                        return sequence.Max() + sequence.Min();
+                    }
+                    else if (sumOfSequence > expectedSum)
+                    {
+                        sequence.RemoveAt(0);
+                    }
+                }
+            }
+
+            return long.MinValue;
+        }
+    }
+}
---
+++ Day9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AdventOfCode2020.Day9.Challenge;
+
+namespace AdventOfCode2020.Day9
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            long invalidNumber = XMASEncription.CheckEncription(25);
+            Console.WriteLine($"First Challenge: {invalidNumber}");
+            Console.WriteLine($"Second Challenge: {XMASEncription.FindSumOfSequenceGapWithSum(invalidNumber)}");
+        }
+    }
+}
